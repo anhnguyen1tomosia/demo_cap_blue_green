51881ec533171fa2a7289c313725d1b4789d348b
Anh Nguyen <miyamoto.anh@gmail.com>
2022-07-23 16:09:54 +0700

    first commit

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -9,7 +9,7 @@ worker_processes 2
 timeout 30
 
 # Set up socket location
-listen "#{shared_dir}/sockets/unicorn.sock", :backlog => 64
+listen "#{shared_dir}/tmp/sockets/unicorn.sock", :backlog => 64
 
 # Logging
 stderr_path "#{shared_dir}/log/unicorn.stderr.log"
